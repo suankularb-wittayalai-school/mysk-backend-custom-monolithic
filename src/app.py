03a685c804c4a905f7c3399fb3191d157d2a79bb
Jimmy_Tempest <smartwattana@gmail.com>
2022-03-28 20:23:48 +0700

    feat: add database data to the app

--- src/app.py
+++ src/app.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+import uvicorn
 from starlette.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+from utils.database import engine
+
 
 app = FastAPI()
 
 cors = CORSMiddleware(app, allow_origins=["*"])
 
+con = engine.connect()
+
 
 @app.get("/")
 def healthCheck():
     return {"status": "OK"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=os.environ.get("HOST"), port=int(os.environ.get("PORT")))
--- src/utils/database.py
+++ src/utils/database.py
@@ -1,9 +1,19 @@
 from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy import (
+    Table,
+    Column,
+    Integer,
+    String,
+    MetaData,
+    ForeignKey,
+    DateTime,
+    Boolean,
+    Constraint,
+)
 from sqlalchemy import inspect
 from dotenv import load_dotenv
 
-from types.student_teacher.people import People
+# from types.student_teacher.person import Person
 import os
 
 load_dotenv()
@@ -11,8 +21,53 @@ load_dotenv()
 metadata = MetaData()
 engine = create_engine(os.environ.get("DATABASE_URL"))
 
+contact_types = Table(
+    "contact_types",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String(50), nullable=False),
+    Column("type", String(50), nullable=False),
+    Column("value", String(50), nullable=False),
+)
+
+person_contact_types = Table(
+    "person_contact_types",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("person_id", Integer, ForeignKey("person.id")),
+    Column("contact_type_id", Integer, ForeignKey("contact_types.id")),
+)
+
 person = Table(
     "person",
     metadata,
     Column("id", Integer, primary_key=True),
+    Column("prefix_th", String),
+    Column("first_name_th", String),
+    Column("middle_name_th", String, nullable=True),
+    Column("last_name_th", String),
+    Column("prefix_en", String),
+    Column("first_name_en", String),
+    Column("middle_name_en", String, nullable=True),
+    Column("last_name_en", String),
+    Column("birthdate", DateTime),
+    Column("citizen_id", String),
 )
+
+student = Table(
+    "student",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("person_id", Integer, ForeignKey("person.id")),
+    Column("student_id", String),
+)
+
+teacher = Table(
+    "teacher",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("person_id", Integer, ForeignKey("person.id")),
+    Column("teacher_id", String),
+)
+
+metadata.create_all(engine)
