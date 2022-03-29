f8b48c97127be90be6e8aa600f2cda70d0889786
CCSleep <captureme257@gmail.com>
2022-03-30 03:17:26 +0700

    Restructure + Add and Read Student test

--- src/app.py
+++ src/app.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
+import uvicorn
 from starlette.middleware.cors import CORSMiddleware
+
+from dotenv import load_dotenv
+import os
+
+from utils import models
+from utils.database import engine
+
+from routes.v1 import V1
+
+load_dotenv()
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
+cors = CORSMiddleware(app, allow_origins=["*"])
 
 @app.get("/")
-def healthCheck():
-    return {"status": "OK"}
+def health_check():
+    return {"success": True}
 
+app.include_router(V1, prefix="/api/v1")
+if __name__ == "__main__":
+    uvicorn.run("app:app", host=os.environ.get("HOST"), port=int(os.environ.get("PORT")), reload=True)
